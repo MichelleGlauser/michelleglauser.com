a0cdc663a96791b7525d465c9077c3839cee2797
Michelle Glauser <michelleglauser@gmail.com>
2014-11-10 10:33:57 -0800

    Set up database and superuser.

--- michelleglauser/settings.py
+++ michelleglauser/settings.py
@@ -58,8 +58,9 @@ WSGI_APPLICATION = 'michelleglauser.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'michelleglauser_com',
+        'HOST': 'localhost',
     }
 }
 
