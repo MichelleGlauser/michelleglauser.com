479d20f948dfac56ffb061b8ba07a33ec4ff1f1c
Michelle Glauser <michelleglauser@gmail.com>
2016-02-05 10:59:44 -0800

    Add TechTonic view, URL, and template with deck.

--- michelleglauser/urls.py
+++ michelleglauser/urls.py
@@ -11,4 +11,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'mg_app.views.index', name='home'),
+    url(r'^techtonic/', 'mg_app.views.techtonic', name='techtonic'),
 )
