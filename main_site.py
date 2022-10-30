a648e59f66b6895e244221d1fc026920edc542fd
MichelleGlauser <michelleglauser@gmail.com>
2022-10-29 19:15:05 -0700

    Create main_site.py file for Flask conversion.

---
+++ main_site.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+import pendulum
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, url_for
+from flask_sslify import SSLify
+
+load_dotenv(find_dotenv(usecwd=True))
+
+app = Flask(__name__)
+sslify = SSLify(app)
+
+
+# MAIN HANDLERS
+@app.route("/")
+def render_home_page():
+    """
+    Renders the home page from jinja2 template
+    """
+    try:
+        return render_template("index.html")
+    except BaseException:
+        return render_template("index.html")
+
+if __name__ == "__main__":
+    app.debug = False
+    # app.run(host='0.0.0.0', port=9999)
